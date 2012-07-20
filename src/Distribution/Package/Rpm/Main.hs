8a9e6403fc90be6ff3432a02ee51892d8f06ccb9
Jens Petersen <juhp@community.haskell.org>
2012-07-20 19:07:32 +0900

    Rpm/Main.hs: implement arg parsing for dir, filepath, pkg, pkg-ver
    - for pkg or pkgver (nonexistant path without slash), use cabal unpack if necessary
    - for file, check it has .cabal extension

--- src/Distribution/Package/Rpm/Main.hs
+++ src/Distribution/Package/Rpm/Main.hs
@@ -16,13 +16,44 @@ module Distribution.Package.Rpm.Main where
 import Distribution.PackageDescription.Parse (readPackageDescription)
 import Distribution.Package.Rpm (rpm)
 import Distribution.Package.Rpm.Setup (RpmFlags (..), parseArgs)
-import Distribution.Simple.Utils (defaultPackageDesc)
+import Distribution.Simple.Utils (defaultPackageDesc, findPackageDesc)
+import Control.Monad (unless)
+import Data.Char (isDigit)
+import System.Directory (doesDirectoryExist, doesFileExist)
 import System.Environment (getArgs)
+import System.FilePath.Posix (takeExtension)
+import System.Process (readProcess, system)
 
 main :: IO ()
-
-main = do opts <- getArgs >>= parseArgs
+main = do (opts, args) <- getArgs >>= parseArgs
           let verbosity = rpmVerbosity opts
-          descPath <- defaultPackageDesc verbosity
+          descPath <- if (null args) then defaultPackageDesc verbosity else findCabalFile  $ head args
           pkgDesc <- readPackageDescription verbosity descPath
           rpm pkgDesc opts
+
+findCabalFile :: FilePath -> IO FilePath
+findCabalFile path = do
+  isdir <- doesDirectoryExist path
+  if isdir
+    then findPackageDesc path
+    else do
+      isfile <- doesFileExist path
+      if (not isfile)
+        then if (notElem '/' path)
+             then tryUnpack path
+             else error $ path ++ ": No such file or directory"
+        else if (takeExtension path /= ".cabal")
+             then error $ path ++ ": file should have .cabal extension file."
+             else return path
+    
+tryUnpack :: String -> IO FilePath
+tryUnpack pkg = do
+  pkgver <- if (isDigit $ last pkg) then return pkg
+            else do     
+              pkgs <- readProcess "cabal" ["list", "--simple-output", pkg] []
+              return $ last $ lines pkgs
+  isdir <- doesDirectoryExist pkgver
+  unless isdir $ do
+    _ <- system $ "cabal unpack " ++ pkgver
+    return ()
+  findPackageDesc pkgver
