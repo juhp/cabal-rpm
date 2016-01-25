6a2028882d8155c6e693ad2d8a5a1639ea451568
Jens Petersen <petersen@redhat.com>
2016-01-25 18:32:05 +0900

    bump to 0.9.9

--- src/Dependencies.hs
+++ src/Dependencies.hs
@@ -1,6 +1,6 @@
 -- |
 -- Module      :  Dependencies
--- Copyright   :  (C) 2012-2015  Jens Petersen
+-- Copyright   :  (C) 2012-2016  Jens Petersen
 --
 -- Maintainer  :  Jens Petersen <petersen@fedoraproject.org>
 -- Stability   :  alpha
