d4ffc91137a135b6cb38ad220cda224a77da796c
Jens Petersen <petersen@redhat.com>
2020-01-15 16:49:59 +0800

    use curl library instead of stackage-query as fallback to http-client

--- src/Stackage.hs
+++ src/Stackage.hs
@@ -22,25 +22,18 @@ module Stackage (
   ) where
 
 
-#ifdef HTTPS
+#ifdef CURL
+import Network.Curl
+#else
 import qualified Data.ByteString.Char8 as B
-import Data.List (isPrefixOf)
-import Data.Maybe (fromMaybe, mapMaybe)
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
-import System.FilePath (takeFileName)
-import SimpleCmd ((+-+), removePrefix)
-#else
-#if (defined(MIN_VERSION_base) && MIN_VERSION_base(4,8,0))
-#else
-import Control.Applicative ((<$>))
 #endif
 import Data.Maybe (fromMaybe)
-import SimpleCmd ((+-+), cmdMaybe)
-import SysCmd (optionalProgram)
-#endif
 import Distribution.Text (display)
 import SimpleCabal (PackageIdentifier(..), PackageName)
+import SimpleCmd ((+-+), removePrefix)
+import System.FilePath (takeFileName)
 import Types
 
 defaultLTS :: Stream
@@ -48,35 +41,23 @@ defaultLTS = LTS "13"
 
 stackageList :: Stream -> PackageName -> IO (Maybe PackageIdentifier)
 stackageList stream pkg = do
-#ifdef HTTPS
-  mgr <- newManager tlsManagerSettings
-  let pkgurl = topurl ++ (showStream stream) ++ "/package/"
-  req <- parseRequest $ pkgurl ++ display pkg
-  hist <- responseOpenHistory req mgr
-  let redirs = mapMaybe (lookup "Location" . responseHeaders . snd) $ hrRedirects hist
-  if null redirs
-    then return Nothing
-    else do
-    let loc = B.unpack $ last redirs
-    if topurl `isPrefixOf` loc
-      then
-      let ver = (readVersion . removePrefix (display pkg ++ "-") . takeFileName) loc
-      in return $ Just (PackageIdentifier pkg ver)
-      else return Nothing
+  let pkgurl = topurl ++ showStream stream ++ "/package/" ++ display pkg
+  mloc <- do
+#ifdef CURL
+    withCurlDo $
+      (lookup "location" . reverse . snd) <$> curlHead pkgurl [CurlFollowLocation True]
+#else
+    mgr <- newManager tlsManagerSettings
+    req <- parseRequest pkgurl
+    resp <- httpNoBody req mgr
+    return $ (fmap B.unpack . lookup "Location" . responseHeaders) resp
+#endif
+  let mver =
+        readVersion . removePrefix (display pkg ++ "-") . takeFileName <$> mloc
+  return $ PackageIdentifier pkg <$> mver
   where
     topurl :: String
     topurl = "https://www.stackage.org/"
-#else
-  -- check for stackage-query
-  haveStackage <- optionalProgram "stackage"
-  if haveStackage
-    then do
-    mver <- cmdMaybe "stackage" ["package", show stream, display pkg]
-    return $ PackageIdentifier pkg . readVersion <$> mver
-    else do
-    putStrLn "'cabal install stackage-query' to check against Stackage LTS"
-    return Nothing
-#endif
 
 latestStackage :: Maybe Stream -> PackageName -> IO (Maybe PackageIdentifier)
 latestStackage mstream pkg = do
