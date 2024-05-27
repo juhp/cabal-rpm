9f971d9688bec0fced1fce5468b36ba56c28c85f
Jens Petersen <juhpetersen@gmail.com>
2024-05-27 19:44:42 +0800

    Dependencies: ignore pthread since it is part of glibc

--- src/Dependencies.hs
+++ src/Dependencies.hs
@@ -123,6 +123,8 @@ buildToolDepends' _ =
 data QueryBackend = Rpm | Repoquery deriving Eq
 
 resolveLib :: String -> IO (Maybe String)
+-- nothing provides libpthread.so
+resolveLib "pthread" = return Nothing
 resolveLib lib = do
   lib64 <- doesDirectoryExist "/usr/lib64"
   let libsuffix = if lib64 then "64" else ""
